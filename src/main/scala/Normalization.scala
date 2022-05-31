b8c7d3252f0475b9ec87318534c22053a6c77573
k.jedra <kozukj@gmail.com>
2022-05-31 18:16:45 +0200

    Not finished

---
+++ src/main/scala/Normalization.scala
@@ -0,0 +1,10 @@
+object Normalization extends App {
+
+  import scala.io.Source
+
+  val fileReader = Source.fromFile("input.txt").getLines
+
+  // to check if calculation is correct
+  // ((x - min) * 2 / max - min) - 1
+
+}
